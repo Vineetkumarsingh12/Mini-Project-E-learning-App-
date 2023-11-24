6b59030db41c685aad9b9ec63e9c339746421a5c
vineet <vineetkumarsingh341@gmail.com>
2023-11-24 19:15:33 +0530

    complete1

---
+++ src/data/navbar-links.js
@@ -0,0 +1,24 @@
+export const NavbarLinks = [
+  {
+    title: "Home",
+    path: "/",
+  },
+  {
+    title: "Catalog",
+    // path: '/catalog',
+  },
+  {
+    title: "About Us",
+    path: "/about",
+  },
+  {
+    title: "Contact Us",
+    path: "/contact",
+  },
+  {
+    title:"Game",
+  },
+  {
+    title:"Interenship",
+  }
+];
